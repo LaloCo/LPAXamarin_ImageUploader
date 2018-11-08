68b707f04ce92805badf074b240fdd524eca9cce
Eduardo Rosas <eduardorosas@MacBook-Pro-de-Eduardo.local>
2018-11-08 08:07:09 -0600

    Permissions for accessing pictures added

---
+++ ImageUploader/ImageUploader/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageUploader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if(!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "Not supported", "Ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if(selectedImageFile == null)
+            {
+                await DisplayAlert("Error", "No image selected", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+    }
+}
